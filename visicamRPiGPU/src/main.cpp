243fca0397eb343fc5ba4f58fa5b64640b4cd5fd
FroChr123 <frochr123@trash-mail.com>
2015-05-28 17:55:54 +0200

    Some text changes

--- visicamRPiGPU/src/main.cpp
+++ visicamRPiGPU/src/main.cpp
@@ -30,7 +30,7 @@ int main(int argc, char *argv[])
     // Quit if argument count does not match
     if (argc != 7)
     {
-        printf("\n####### USAGE: #######\n\n");
+        printf("\n####### USAGE: #######\n");
         printf("Argument 1: (int) Width pixel\n");
         printf("Argument 2: (int) Height pixel\n");
         printf("Argument 3: (int) Refresh time in seconds (captured original output image, homography input matrix)\n");
@@ -43,26 +43,27 @@ int main(int argc, char *argv[])
     }
 
     // Information for program
-    printf("\n####### PROGRAM:#######\n\n");
-    printf("This program is designed to use the Raspberry Pi 2 hardware acceleration APIs.\n");
+    printf("\n####### PROGRAM:#######\n");
+    printf("This program is designed to use the Raspberry Pi 2 (OS: Raspbian) hardware acceleration APIs.\n");
     printf("OpenGL ES 2.0 and OpenMax IL are currently used for this purpose and it might be adaptable to other systems.\n");
     printf("For more details please visit the project page (https://github.com/FroChr123/visicamRPiGPU).\n");
 
     // Information for paths
-    printf("\n####### PATHS:#######\n\n");
+    printf("\n####### PATHS:#######\n");
     printf("You should use absolute paths in the input parameters.\n");
     printf("It is advised to use paths to the shared memory /run/shm because there are a lot of file read / write operations in this program.\n");
 
     // Information for camera options
-    printf("\n####### CAMERA OPTIONS:#######\n\n");
+    printf("\n####### CAMERA OPTIONS:#######\n");
     printf("There are a lot of different camera options.\n");
     printf("You can change all used camera options in the defines of the header file.\n");
 
     // Information for memory split
-    printf("\n####### MEMORY SPLIT:#######\n\n");
+    printf("\n####### MEMORY SPLIT:#######\n");
     printf("For the maximum allowed resolution of 1920 x 1072 you need to have a memory split in raspi-config of around 140 MB.\n");
     printf("The resolution 1280 x 720 is recommended because of processing speed and it runs with 128 MB memory split in raspi-config.\n");
-    printf("You can get any kind of strange behaviour if there is not enough GPU memory such as white screens or errors in buffer allocation.\n");
+    printf("Recommended image format is 16:9, otherwise you might get weird cropping effects or borders in your images.\n");
+    printf("You can get any kind of strange behaviour if there is not enough GPU memory such as white screens or errors in buffer allocation.\n\n");
 
     // Initialize app
     visicamRPiGPU app;
